1b611cc20157201e89ac08ea17e7960be841bedb
Aaron <aaroncao@ucsb.edu>
2026-09-11 03:07:53 -0700

    Refine homepage interests

--- site/home.ml
+++ site/home.ml
@@ -6,7 +6,7 @@ let body =
   [ heading H1 [ text "Aaron Cao" ]
   ; paragraph
       [ text
-          "I am interested in building and understanding intelligent systems, particularly by drawing on observations about human cognition. But hard problems in general are cool. Current interests: multimodal models, recurrent models, pre-pretraining, programming languages."
+          "I am generally interested in building and understanding intelligent systems, particularly by drawing on observations about human cognition. Specifically, my current interests include multimodal models, recurrent models, pre-pretraining, and programming languages. But hard problems in general are cool."
       ]
   ; Shared.external_links
   ; thematic_break ()
